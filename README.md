a08d4eb24b89a350a1f5b45675f2adfa75bb1a61
Ellie <contact@elliehigh.com>
2025-10-30 23:44:11 -0700

    update

---
+++ README.md
@@ -0,0 +1,1 @@
+nasty nasty krita doesn't ship builds with wayland support
--- flake.nix
+++ flake.nix
@@ -1,39 +1,180 @@
 {
-    description = "Krita Next Nightly";
+  description = "Standalone flake for Krita (Qt6, master branch build)";
 
-    inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
-        flake-utils.url = "github:numtide/flake-utils";
-    };
+  outputs = { self, nixpkgs }:
+  let
+    systems = [ "x86_64-linux" "aarch64-linux" ];
+    rev = "9dd95d08f437c9025ea7da0944927f1cf00e3361";
+    version = "master-${rev}";
+    forEachSystem = f: nixpkgs.lib.genAttrs systems (system: f system);
+  in
+  {
+    packages = forEachSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        lib = pkgs.lib;
 
-    outputs = { self, nixpkgs, flake-utils }:
-        flake-utils.lib.eachDefaultSystem(system:
+        kritaSrc = pkgs.fetchgit {
+          url = "https://invent.kde.org/graphics/krita.git";
+          inherit rev;
+          hash = "sha256-2WKoA8V7dNREVD5FX38GlqEU1SwK9qJWmJXrEbfFdoQ=";
+        };
+
+        krita-unwrapped = pkgs.stdenv.mkDerivation rec {
+          pname = "krita-unwrapped";
+          inherit version;
+
+          src = kritaSrc;
+
+          nativeBuildInputs = with pkgs; [
+            cmake
+            extra-cmake-modules
+            pkg-config
+            python3Packages.sip
+            kdePackages.wrapQtAppsHook
+          ];
+
+          buildInputs = with pkgs; [
+            kdePackages.qttools
+            kdePackages.karchive
+            kdePackages.kconfig
+            kdePackages.kwidgetsaddons
+            kdePackages.kcompletion
+            kdePackages.kcoreaddons
+            kdePackages.kguiaddons
+            kdePackages.ki18n
+            kdePackages.kitemmodels
+            kdePackages.kitemviews
+            kdePackages.kwindowsystem
+            kdePackages.kio
+            kdePackages.kcrash
+            kdePackages.breeze-icons
+            boost
+            libraw
+            fftw
+            eigen
+            exiv2
+            fribidi
+            lcms2
+            gsl
+            openexr
+            lager
+            libaom
+            libheif
+            kdePackages.libkdcraw
+            giflib
+            libjxl
+            mlt
+            openjpeg
+            opencolorio
+            xsimd
+            poppler
+            curl
+            ilmbase
+            immer
+            kseexpr
+            libmypaint
+            libunibreak
+            libwebp
+            qt6.qtbase
+            qt6.qtmultimedia
+            kdePackages.quazip
+            SDL2
+            zug
+            python3Packages.pyqt6
+          ];
+
+          env.NIX_CFLAGS_COMPILE =
+            toString (lib.optional pkgs.stdenv.cc.isGNU "-Wno-deprecated-copy");
+
+          postPatch =
             let
-                pkgs = import nixpkgs { inherit system; };
-                version = "unstable-2025-10-30-113ee7c896";
-                pname = "krita-next";
-            in {
-                packages.krita-next = pkgs.appimageTools.wrapType2 rec {
-                    inherit pname;
-                    inherit version;
+              pythonPath = pkgs.python3Packages.makePythonPath (with pkgs.python3Packages; [
+                sip
+                setuptools
+              ]);
+            in
+            ''
+              substituteInPlace cmake/modules/FindSIP.cmake \
+                --replace 'PYTHONPATH=''${_sip_python_path}' 'PYTHONPATH=${pythonPath}'
+              substituteInPlace cmake/modules/SIPMacros.cmake \
+                --replace 'PYTHONPATH=''${_krita_python_path}' 'PYTHONPATH=${pythonPath}'
 
-                    src = pkgs.fetchurl {
-                        name = "krita-next-${version}.AppImage";
-                        url =
-                            "https://cdn.kde.org/ci-builds/graphics/krita/master/linux/krita-5.3.0-prealpha-113ee7c896-x86_64.AppImage";
-                        sha256 = "sha256-ldPCa8HrredCQx7te8TlcP5ex3eXUmcAffwFC9MEY2c=";
-                    };
+              substituteInPlace plugins/impex/jp2/jp2_converter.cc \
+                --replace '<openjpeg.h>' '<${pkgs.openjpeg.dev}/include/openjpeg-2.5/openjpeg.h>'
+            '';
 
-                    extraPkgs = pkgs: [];
+          postFixup = 
+            let
+              pyRuntimePath = pkgs.python3Packages.makePythonPath (with pkgs.python3Packages; [
+                pyqt6
+                sip
+              ]);
+            in
+              ''
+                wrapQtApp "$out/bin/krita" \
+                  --prefix PYTHONPATH : "${pyRuntimePath}" \
+                  --set KRITA_PLUGIN_PATH "$out/lib/kritaplugins" \
+                  --set QT_QPA_PLATFORM "wayland"
+              '';
 
-                    extraInstallCommands = ''
-                        wrapProgram $out/bin/${pname} \
-                            --set QT_QPA_PLATFORM "wayland"
-                    '';
-                };
+          cmakeBuildType = "RelWithDebInfo";
 
-                packages.default = self.packages.${system}.krita-next;
-            }
-        );
+          cmakeFlags = let
+            sp = pkgs.python3Packages.python.sitePackages;
+            p6 = pkgs.python3Packages.pyqt6;
+          in [
+            "-DBUILD_WITH_QT6=ON"
+            "-DQT_MAJOR_VERSION=6"
+            "-DPYQT6_SIP_DIR=${p6}/${sp}/PyQt6/bindings"
+            "-DPYQT_SIP_DIR_OVERRIDE=${p6}/${sp}/PyQt6/bindings"
+            "-DBUILD_KRITA_QT_DESIGNER_PLUGINS=ON"
+          ];
+
+          meta = with lib; {
+            description  = "Krita painting application built from master branch (Qt6)";
+            homepage     = "https://krita.org/";
+            mainProgram  = "krita";
+            platforms    = platforms.linux;
+            license      = licenses.gpl3Only;
+          };
+        };
+      in
+      {
+        default = krita-unwrapped;
+        krita-unwrapped = krita-unwrapped;
+      }
+    );
+
+    apps = forEachSystem (system: let pkg = self.packages.${system}.krita-unwrapped; in {
+      default = {
+        type = "app";
+        program = "${pkg}/bin/krita";
+      };
+      krita = {
+        type = "app";
+        program = "${pkg}/bin/krita";
+      };
+    });
+
+    devShells = forEachSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            cmake
+            extra-cmake-modules
+            pkg-config
+            python3Packages.sip
+          ];
+          buildInputs = with pkgs; [
+            python3Packages.pyqt6
+          ];
+        };
+      }
+    );
+  };
 }
+
